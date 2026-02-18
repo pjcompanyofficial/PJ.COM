6cfa250c15f94b57422726b6ca40ccf9ed69bd80
pjcompanyofficial <pjcompanyofficial@gmail.com>
2026-02-18 21:13:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,10 @@
-const CACHE="pj-store-v1";
-
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE).then(cache=>{
-return cache.addAll([
-"/",
-"index.html",
-"manifest.json"
-]);
-})
-);
+self.addEventListener("install", event => {
+self.skipWaiting();
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(res=>{
-return res || fetch(e.request);
-})
-);
+self.addEventListener("activate", event => {
+event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
 });
