9f07a31e3ea1fc17717899e435533f9d853f216c
pjcompanyofficial <pjcompanyofficial@gmail.com>
2026-02-18 21:18:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-self.addEventListener("install", event => {
-self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-});
+<script>
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js")
+}
+</script>
