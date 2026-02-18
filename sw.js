94f6e2665e16cfd4450c5b5046c39d8ffa9e85be
pjcompanyofficial <pjcompanyofficial@gmail.com>
2026-02-18 20:59:18 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE="pj-store-v1";
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>{
+return cache.addAll([
+"/",
+"index.html",
+"manifest.json"
+]);
+})
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>{
+return res || fetch(e.request);
+})
+);
+});
